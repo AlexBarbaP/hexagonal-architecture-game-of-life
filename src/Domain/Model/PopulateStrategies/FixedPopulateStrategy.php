0b55840fb51c0ef44ed85ee056788769231b0605
Alex Agile <alexagile@alexagile.com>
2017-12-12 12:42:10 +0100

    GOL12 - Population strategies skeleton

--- src/Domain/Model/PopulateStrategies/FixedPopulateStrategy.php
+++ src/Domain/Model/PopulateStrategies/FixedPopulateStrategy.php
@@ -3,90 +3,15 @@ declare(strict_types=1);
 
 namespace Domain\Model\PopulateStrategies;
 
-use Domain\Exception\InvalidGridException;
-use Domain\Exception\InvalidSizeException;
-use Domain\Model\Cell;
-use Domain\Model\CellStatus;
-use Domain\Model\Coordinate;
-
 final class FixedPopulateStrategy implements PopulateStrategyInterface
 {
-    /**
-     * @var array
-     */
-    private $grid = [];
-
-    /**
-     * @param array $grid
-     *
-     * @throws InvalidGridException
-     */
-    public function __construct(array $grid)
-    {
-        if (count($grid) <= 0) {
-            throw new InvalidGridException();
-        }
-
-        if (count($grid[0]) <= 0) {
-            throw new InvalidGridException();
-        }
-
-        $this->grid = $grid;
-    }
-
     /**
      * @param array $boardGrid
      *
      * @return array
-     *
-     * @throws InvalidSizeException
      */
     public function populate(array $boardGrid): array
     {
-        for ($y = 0; $y < count($boardGrid); $y++) {
-            $boardGrid = $this->populateRow($boardGrid, $y);
-        }
-
-        return $boardGrid;
-    }
-
-    /**
-     * @param array $grid
-     * @param       $rowIndex
-     *
-     * @return array
-     *
-     * @throws InvalidSizeException
-     */
-    private function populateRow(array $grid, $rowIndex): array
-    {
-        for ($x = 0; $x < count($grid[$rowIndex]); $x++) {
-            $grid = $this->populateCell($grid, $rowIndex, $x);
-        }
-
-        return $grid;
-    }
-
-    /**
-     * @param array $grid
-     * @param       $rowIndex
-     * @param       $cellIndex
-     *
-     * @return array
-     *
-     * @throws InvalidSizeException
-     */
-    private function populateCell(array $grid, $rowIndex, $cellIndex): array
-    {
-        if (!isset($this->grid[$rowIndex][$cellIndex])) {
-            throw new InvalidSizeException();
-        }
-
-        $coordinate = new Coordinate($cellIndex, $rowIndex);
-        $cellStatus = new CellStatus($this->grid[$rowIndex][$cellIndex]);
-
-        $grid[$rowIndex][$cellIndex] = new Cell($coordinate, $cellStatus);
-
-        return $grid;
+        // TODO: Implement populate() method.
     }
 }
--- src/Domain/Model/Simulation.php
+++ src/Domain/Model/Simulation.php
@@ -3,32 +3,17 @@ declare(strict_types=1);
 
 namespace Domain\Model;
 
-use Domain\Model\PopulateStrategies\FixedPopulateStrategy;
-use Domain\Model\PopulateStrategies\PopulateStrategyInterface;
-use Domain\Model\Rules\DeadSimulationRule;
-use Domain\Model\Rules\PopulateSimulationRule;
-use Domain\Model\Rules\RuleInterface;
-use Domain\Model\Rules\SurvivalSimulationRule;
-
 final class Simulation
 {
     /** @var Board */
     private $board;
 
-    /** @var RuleInterface[] */
-    private $rules;
-
     /**
-     * @param Size                      $size
-     * @param PopulateStrategyInterface $populateStrategy
+     * @param Size $size
      */
-    public function __construct(
-        Size $size,
-        PopulateStrategyInterface $populateStrategy
-    ) {
-        $this->board = new Board($size, $populateStrategy);
-
-        $this->addRules();
+    public function __construct(Size $size)
+    {
+        $this->board = new Board($size);
     }
 
     /**
@@ -38,37 +23,7 @@ final class Simulation
     {
         $newGrid = $this->iterateBoard($this->getBoard()->getGrid());
 
-        $fixedPopulateStrategy = new FixedPopulateStrategy($newGrid);
-
-        $this->board = new Board($this->board->getSize(), $fixedPopulateStrategy);
-    }
-
-    /**
-     * @return Board
-     */
-    public function getBoard(): Board
-    {
-        return $this->board;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isCompleted(): bool
-    {
-        $anyPopulatedCell = $this->getBoard()->isAnyPopulatedCell();
-
-        return !$anyPopulatedCell;
-    }
-
-    /**
-     *
-     */
-    private function addRules(): void
-    {
-        $this->rules[] = new DeadSimulationRule();
-        $this->rules[] = new SurvivalSimulationRule();
-        $this->rules[] = new PopulateSimulationRule();
+        $this->board = $this->board->setGrid($newGrid);
     }
 
     /**
@@ -76,7 +31,7 @@ final class Simulation
      *
      * @return array
      */
-    private function iterateBoard(array $grid): array
+    private function iterateBoard(array &$grid): array
     {
         $newGrid = [];
 
@@ -85,9 +40,11 @@ final class Simulation
 
             /** @var Cell $cell */
             foreach ($row as $cell) {
+                $coordinate = $cell->getCoordinate();
+
                 $nextIterationCellStatus = $this->getNextIterationCellStatus($cell);
 
-                $newGrid[count($newGrid) - 1][] = $nextIterationCellStatus();
+                $newGrid[count($newGrid) - 1][] = new Cell($coordinate, $nextIterationCellStatus);
             }
         }
 
@@ -101,31 +58,14 @@ final class Simulation
      */
     private function getNextIterationCellStatus(Cell $cell): CellStatus
     {
-        $cellCurrentStatus = $cell->getCellStatus();
-
-        $cellNeighbors = $this->board->getNeighbors($cell->getCoordinate());
-
-        $newCellStatus = $this->executeRules($cellCurrentStatus, $cellNeighbors);
-
-        return $newCellStatus;
+        return new CellStatus(Cell::UNPOPULATED);
     }
 
     /**
-     * @param CellStatus $cellStatus
-     * @param int        $cellNeighbors
-     *
-     * @return CellStatus
+     * @return Board
      */
-    private function executeRules(CellStatus $cellStatus, int $cellNeighbors): CellStatus
+    public function getBoard(): Board
     {
-        foreach ($this->rules as $rule) {
-            if ($rule->match($cellStatus, $cellNeighbors)) {
-                $newCellStatus = $rule->execute();
-
-                return $newCellStatus;
-            }
-        }
-
-        return $cellStatus;
+        return $this->board;
     }
 }
